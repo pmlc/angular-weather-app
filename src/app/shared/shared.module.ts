fb10af94af9eebe87c90230310fe7761b4249499
Pierre Le Count <pmlc@iinet.net.au>
2019-03-21 04:17:16 +0000

    Added map

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
+import { environment } from '../../environments/environment';
 
 @NgModule({
+  
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'API - KEY'
+    }),
   ],
   exports: [
-    FormsModule
+    FormsModule,
+    AgmCoreModule
   ],
   declarations: []
 })
